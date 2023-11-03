b55d9331e1434ecbfb49aeb229467a1fd3bc318d
Komediem <gbussola@artfx.fr>
2023-11-04 00:15:47 +0100

    [CODE] - Unit Scriptable Object

---
+++ Assets/Scripts/ScriptableObjectClasses/Unit.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Unit", menuName = "Unit")]
+public class Unit : ScriptableObject
+{
+    public string unitName;
+
+    public int life;
+    public int attack;  
+    public int speed;
+
+    public Image unitIcon;
+
+
+    [Header("Enumerations")]
+    public unitType UnitType;
+    public enum unitType
+    {
+        Fighter,
+        AssaultPlane,
+        Bomber,
+        AntiAerial
+    }
+    public rarity Rarity;
+    public enum rarity
+    {
+        Common,
+        Rare,
+        Legendary
+    }
+}
