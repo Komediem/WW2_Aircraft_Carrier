4bf033f9e8dae8c10b58e5e10b241c81ea02ea4b
Gabbbbbbbbbb <gcochard@artfx.fr>
2024-01-05 02:06:16 +0100

    05/01 - Fixing camera (unfinished)
    [DEV] Modifying the camera game object and its parent.
    [CODE] Modifying the "CameraManager" script + slowly working toward a fix for clamping camera movements !

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,10 +16,26 @@ public class CameraManager : MonoBehaviour
     public Quaternion NewRotation;
     public Vector3 newZoom;
 
+    public Vector3 dragStartPosition;
+    public Vector3 dragCurrentPosition;
+    public Vector3 rotateStartPosition;
+    public Vector3 rotateCurrentPosition;
+
+    [Header ("Testing")]
+    //TESTING
+    public Collider limit;
+    public float MaxLength;
+    public Vector3 offset;
+
+    public float min;
+    public float max;
+    public float clampX = 100;
+    public float clampZ = -100;
+
     // Start is called before the first frame update
     void Start()
     {
-        NewPosition = transform.position; //So that our transforme doesn't automatically default to 0.
+        NewPosition = transform.position; //So that our transform doesn't automatically default to 0.
         NewRotation = transform.rotation;
         newZoom = cameraTransform.localPosition;
     }
@@ -27,6 +44,65 @@ public class CameraManager : MonoBehaviour
     void Update()
     {
         HandleMovementInput();
+        HandleMouseInput();
+        
+        //Clamp of Vector NewPosition x and z.
+        NewPosition.x = Mathf.Clamp(clampX, min, max);
+        Debug.Log(NewPosition.x); //It seems that the Vector NewPosition gets stuck at the "max" of the Clamp.
+                                  //Shoudl I try clamping the transform instead ?
+    }
+
+    void HandleMouseInput()
+    {
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            newZoom += Input.mouseScrollDelta.y * zoomAmount;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float entry;
+
+            if(plane.Raycast(ray, out entry))
+            {
+                dragStartPosition = ray.GetPoint(entry);
+            }
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float entry;
+
+            if (plane.Raycast(ray, out entry))
+            {
+                dragCurrentPosition = ray.GetPoint(entry);
+
+                NewPosition = transform.position + dragStartPosition - dragCurrentPosition;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(2))
+        {
+            rotateStartPosition = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(2))
+        {
+            rotateCurrentPosition = Input.mousePosition;
+
+            Vector3 difference = rotateStartPosition - rotateCurrentPosition;
+
+            rotateStartPosition = rotateCurrentPosition;
+
+            NewRotation *= Quaternion.Euler(Vector3.up * (-difference.x / 5f));
+        }
     }
 
     void HandleMovementInput()
@@ -67,8 +143,9 @@ public class CameraManager : MonoBehaviour
             newZoom -= zoomAmount;
         }
 
-        transform.position = Vector3.Lerp(transform.position, NewPosition, Time.deltaTime * MovementTime);
+
+        transform.position = Vector3.Lerp(transform.position, NewPosition, Time.deltaTime * MovementTime); // NEEDS TO BE CLAMPED (Vector NewPosition)
         transform.rotation = Quaternion.Lerp(transform.rotation, NewRotation, Time.deltaTime * MovementTime);
-        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * MovementTime);
+        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * MovementTime); // NEEDS TO BE CLAMPED
     }
 }
